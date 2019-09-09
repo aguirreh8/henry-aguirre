ecc10e910ebe57e13079ffdde6ba96d0d115abcc
unknown <aguirreh8@gmail.com>
2019-09-08 21:57:48 -0400

    Change email form into send button, delete commented code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,6 @@
 const navBar = document.getElementById("navBar");
 const w = window.innerHeight;
 
-// if (w > 400) {
-//     console.log("Hello!");
-// }
-
-// if (window.scrollY > 400) {
-//     navBar.classList.add("fadeIn");
-//     console.log(window.scrollY);
-// } else {
-//     navBar.classList.remove("fadeIn");
-// }
-
 window.onscroll = function changeClass() {
     let scrollPosY = window.pageYOffset | document.body.scrollTop;
     if(scrollPosY > 400) {
